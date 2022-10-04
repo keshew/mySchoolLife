8578123501350e4d7b52b0f8cfef9a68ba0624cf
keshew <97288599+keshew@users.noreply.github.com>
2022-10-04 14:59:38 +0500

    Initial Commit

---
+++ mySchoolLife/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mySchoolLife
+//
+//  Created by Артём Коротков on 04.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
